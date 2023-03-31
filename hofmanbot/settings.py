37074e8053b800bc8ac0346c86d1b0f94c88a13e
Umid <umid08982@gmail.com>
2023-03-31 19:04:00 +0500

    railway dep2

--- hofmanbot/settings.py
+++ hofmanbot/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ws^e8q^bim5&en5nal*ua7-x3jmg7i647gmmbo@=89btd%p%b+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
